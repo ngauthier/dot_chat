25762fd4aa9ce28e276b74211a1c43c2b4d24fac
Nick Gauthier <ngauthier@gmail.com>
2011-11-22 20:07:08 -0500

    now, with streaming!

--- dot_chat.rb
+++ dot_chat.rb
@@ -1,20 +1,21 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'goliath'
+require 'open3'
 
 class DotChat < Goliath::API
-  use Goliath::Rack::Params 
-  use Goliath::Rack::Validation::RequiredParam, {:key => 'dot'}
+  def on_headers(env, headers)
+    env['dot-in'], env['dot-out'] = Open3.popen2("dot -Tsvg")
+  end
+
+  def on_body(env, data)
+    env['dot-in'].write data
+  end
 
   def response(env)
-    dot = env.params['dot']
-    result = nil
-
-    IO.popen("dot -Tsvg", "r+") do |io|
-      io.write dot
-      io.close_write
-      result = io.read
-    end
+    env['dot-in'].close
+    result = env['dot-out'].read
+    env['dot-out'].close
 
     [200, {}, result]
   end
